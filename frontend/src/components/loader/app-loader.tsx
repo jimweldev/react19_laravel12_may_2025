6cc3f4b064d97ce455b0f93ec7caf54f96279bf8
Jimwel Dizon <jimweldizon.dev@gmail.com>
2025-06-01 13:49:28 +0800

    pwa test

--- frontend/src/components/loader/app-loader.tsx
+++ frontend/src/components/loader/app-loader.tsx
@@ -1,16 +1,13 @@
-import appLogo from '/images/app-logo.jpg';
-import logoLong from '/images/company-logo-long.png';
-
 const AppLoader = () => {
   return (
     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-white">
       {/* center */}
-      <img className="w-32" src={appLogo} alt="Logo" />
+      <img className="w-32" src="/images/app-logo.jpg" alt="Logo" />
 
       {/* bottom */}
       <div className="absolute bottom-8 flex flex-col gap-4">
         <p className="text-muted-foreground text-center font-semibold">from</p>
-        <img className="w-24" src={logoLong} alt="Logo" />
+        <img className="w-24" src="/images/company-logo-long.png" alt="Logo" />
       </div>
     </div>
   );
