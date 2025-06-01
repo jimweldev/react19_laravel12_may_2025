87764e044790440b2b8bb5adbe2c72d2f148aa1d
Jimwel Dizon <jimweldizon.dev@gmail.com>
2025-06-01 12:48:54 +0800

    test

--- frontend/src/components/pwa/install-pwa-button.tsx
+++ frontend/src/components/pwa/install-pwa-button.tsx
@@ -1,54 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import { usePWAInstall } from 'react-use-pwa-install';
 
 const InstallPWAButton = () => {
-  const [deferredPrompt, setDeferredPrompt] =
-    useState<BeforeInstallPromptEvent | null>(null);
-  const [isAppInstalled, setIsAppInstalled] = useState(false);
-
-  useEffect(() => {
-    const handleBeforeInstallPrompt = (e: Event) => {
-      e.preventDefault();
-      setDeferredPrompt(e as BeforeInstallPromptEvent);
-    };
-
-    const handleAppInstalled = () => {
-      setIsAppInstalled(true);
-      setDeferredPrompt(null);
-    };
-
-    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-    window.addEventListener('appinstalled', handleAppInstalled);
-
-    return () => {
-      window.removeEventListener(
-        'beforeinstallprompt',
-        handleBeforeInstallPrompt,
-      );
-      window.removeEventListener('appinstalled', handleAppInstalled);
-    };
-  }, []);
-
-  const handleInstallClick = async () => {
-    if (deferredPrompt) {
-      deferredPrompt.prompt();
-      const { outcome } = await deferredPrompt.userChoice;
-      if (outcome === 'accepted') {
-        setDeferredPrompt(null);
-      }
-    }
-  };
+  const install = usePWAInstall();
 
   return (
-    <div>
-      {!isAppInstalled && deferredPrompt && (
-        <button
-          onClick={handleInstallClick}
-          className="rounded bg-blue-600 p-2 text-white"
-        >
-          Install App
-        </button>
-      )}
-    </div>
+    <header>
+      <h1>My app</h1>
+      {install && <button onClick={install}>Install</button>}
+    </header>
   );
 };
 
